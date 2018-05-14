19d3d5f3bf67f50e932ab987cf4cacc1da793aa5
201411317 <whalsrb1226@naver.com>
2018-05-14 10:09:55 +0900

    Retrofit Client

---
+++ app/src/main/java/com/rorrim/mang/smartmirror/Network/RetrofitClient.java
@@ -0,0 +1,39 @@
+package com.rorrim.mang.smartmirror.Network;
+
+import com.rorrim.mang.smartmirror.Interface.Connectable;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient implements Connectable {
+    private static RetrofitClient instance;
+    private Retrofit retrofit;
+    private OkHttpClient client;
+
+    public RetrofitClient() {
+        OkHttpClient.Builder okHttpBuilder = new OkHttpClient.Builder();
+
+        client = okHttpBuilder.build();
+
+        retrofit = new Retrofit.Builder().baseUrl(baseURL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+    }
+    public static RetrofitClient getInstance() {
+        if (instance == null) {
+            instance = new RetrofitClient();
+            /*
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(baseUrl)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();*/
+        }
+        return instance;
+    }
+
+    public Retrofit getRetrofit() {
+        return retrofit;
+    }
+}
