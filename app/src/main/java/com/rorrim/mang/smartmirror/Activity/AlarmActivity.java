d312b39521507d5009bff14bed77557bddb19f60
heaeat <heaeat@naver.com>
2018-06-06 16:23:54 +0900

    Edit News Activity

--- app/src/main/java/com/rorrim/mang/smartmirror/Activity/AlarmActivity.java
+++ app/src/main/java/com/rorrim/mang/smartmirror/Activity/AlarmActivity.java
@@ -9,6 +9,10 @@ import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.RadioGroup;
+import android.widget.SpinnerAdapter;
+import android.widget.Toast;
 
 import com.rorrim.mang.smartmirror.Auth.AuthManager;
 import com.rorrim.mang.smartmirror.Data.DataManager;
@@ -30,16 +34,51 @@ public class AlarmActivity extends Activity {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_alarm);
         binding.setActivity(this);
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         binding.alarmMenuLayout.setSwitch();
+
     }
 
     public void sendCategoryName()   {
-        sendCategory(AuthManager.getInstance().getUser().getUid(), binding.alarmCtgSpinner.getSelectedItem().toString());
+
+        if(binding.upRadioGroup.getCheckedRadioButtonId() == -1 && binding.downRadioGroup.getCheckedRadioButtonId() == -1){
+            Toast.makeText(this, "관심분야를 하나 선택해주세요!", Toast.LENGTH_SHORT).show();
+        }else if(binding.upRadioGroup.getCheckedRadioButtonId() != -1){
+            sendCategory(AuthManager.getInstance().getUser().getUid(), String.valueOf(binding.upRadioGroup.getCheckedRadioButtonId()));
+        }else if(binding.downRadioGroup.getCheckedRadioButtonId() != -1){
+            sendCategory(AuthManager.getInstance().getUser().getUid(), String.valueOf(binding.downRadioGroup.getCheckedRadioButtonId()));
+        }
+
+    }
+
+    public void downClear(){
+        binding.upRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(group.getId() == R.id.up_radio_group){
+                    if(binding.downRadioGroup.getCheckedRadioButtonId() != -1) binding.downRadioGroup.clearCheck();
+                }
+            }
+        }
+        );
+    }
+
+    public void upClear(){
+        binding.downRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(group.getId() == R.id.down_radio_group){
+                    if(binding.upRadioGroup.getCheckedRadioButtonId() != -1) binding.upRadioGroup.clearCheck();
+                }
+            }
+        });
+
+
     }
 
     private void sendCategory(String uid, String category) {
@@ -50,7 +89,8 @@ public class AlarmActivity extends Activity {
             public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
                 // you  will get the reponse in the response parameter
                 if (response.isSuccessful()) {
-
+                    Toast.makeText(getApplicationContext(), "Success set category.",
+                            Toast.LENGTH_SHORT).show();
                 } else {
                     int statusCode = response.code();
                 }
