6e9d2f9e316de5dbea214eaf3de1ed4a760e1334
201411317 <whalsrb1226@naver.com>
2018-06-07 17:25:17 +0900

    Save WeatherActivity

--- app/src/main/java/com/rorrim/mang/smartmirror/Activity/WeatherActivity.java
+++ app/src/main/java/com/rorrim/mang/smartmirror/Activity/WeatherActivity.java
@@ -17,7 +17,6 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.Switch;
 
-import com.rorrim.mang.smartmirror.Data.DataManager;
 import com.rorrim.mang.smartmirror.R;
 import com.rorrim.mang.smartmirror.databinding.ActivityWeatherBinding;
 
@@ -30,13 +29,23 @@ public class WeatherActivity extends Activity implements View.OnClickListener {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_weather);
-        binding.weatherMenuLayout.setSwitch();
+        Switch sw = findViewById(R.id.menu_switch);
+        sw.setChecked(getState());
+        sw.setOnClickListener(this);
+
         setImage();
     }
 
     @Override
     public void onClick(View v)   {
         setImage();
+    }
+
+    public Boolean getState() {
+        boolean temp;
+        SharedPreferences prefs = getSharedPreferences("Activity.WeatherActivity", MODE_PRIVATE);
+        temp = prefs.getBoolean("myState", false);
+        return temp;
     }
 
     public void setImage()  {
